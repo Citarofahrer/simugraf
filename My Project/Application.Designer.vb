c91d26f76fa7a810dc26f2f65726949f384019fd
Citarofahrer <48877496+Citarofahrer@users.noreply.github.com>
2021-03-07 22:22:27 +0100

    first steps

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.simugraf.Form1
+            Me.MainForm = Global.simugraf.frmeditor
         End Sub
     End Class
 End Namespace
---
+++ frmeditor.vb
@@ -0,0 +1,3 @@
+﻿Public Class frmeditor
+
+End Class
